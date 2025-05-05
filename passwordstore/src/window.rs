21b275c15c35e72b94f43226ae2f252f8dd6d505
nick <noobping@users.noreply.github.com>
2025-05-05 21:49:48 +0200

    Add password and text file pages

--- passwordstore/src/window.rs
+++ passwordstore/src/window.rs
@@ -24,19 +24,45 @@ use gtk::{gio, glib};
 use passcore::PassStore;
 
 mod imp {
+    use gettextrs::gettext;
+
     use super::*;
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
     #[template(resource = "/io/github/noobping/PasswordStore/window.ui")]
     pub struct PasswordstoreWindow {
         #[template_child]
-        pub list: TemplateChild<gtk::ListBox>,
+        pub window_title: TemplateChild<adw::WindowTitle>,
 
         #[template_child]
         pub toast_overlay: TemplateChild<adw::ToastOverlay>,
 
         #[template_child]
+        pub nav: TemplateChild<adw::NavigationView>,
+
+        // ① List page
+        #[template_child]
+        pub list_page: TemplateChild<adw::NavigationPage>,
+
+        #[template_child]
         pub search_entry: TemplateChild<gtk::SearchEntry>,
+
+        #[template_child]
+        pub list: TemplateChild<gtk::ListBox>,
+
+        // ② Ask for password page
+        #[template_child]
+        pub ask_page: TemplateChild<adw::NavigationPage>,
+
+        #[template_child]
+        pub password_entry: TemplateChild<gtk::Entry>,
+
+        // ③ Text editor page
+        #[template_child]
+        pub text_page: TemplateChild<adw::NavigationPage>,
+
+        #[template_child]
+        pub text_view: TemplateChild<gtk::TextView>,
     }
 
     impl PasswordstoreWindow {
@@ -56,6 +82,35 @@ mod imp {
                 label.set_valign(gtk::Align::Center);
                 label.set_vexpand(false);
                 list.append(&label);
+            }
+        }
+
+        pub fn show_toast(&self, message: &str) {
+            let overlay = self.toast_overlay.clone();
+            overlay.add_toast(adw::Toast::new(message));
+        }
+
+        pub fn get_path(&self) -> String {
+            let subtitle = self.window_title.subtitle().to_string();
+            let values = ["Manage your passwords", &gettext("subtitle")];
+            return if subtitle.is_empty() || values.contains(&subtitle.as_str()) {
+                String::from("")
+            } else {
+                subtitle
+            };
+        }
+
+        pub fn set_path(&self, path: String) {
+            if path.is_empty() {
+                let translated = &gettext("subtitle");
+                let subtitle = if translated.is_empty() {
+                    &"Manage your passwords".to_string()
+                } else {
+                    translated
+                };
+                self.window_title.set_subtitle(&subtitle);
+            } else {
+                self.window_title.set_subtitle(path.as_str());
             }
         }
     }
@@ -91,6 +146,16 @@ mod imp {
             add_action.connect_activate(move |_, _| obj_clone.open_new_password());
             obj.add_action(&add_action);
 
+            let obj_clone = obj.clone();
+            let add_action = gio::SimpleAction::new("ask-passphrase", None);
+            add_action.connect_activate(move |_, _| obj_clone.ask_for_passphrase());
+            obj.add_action(&add_action);
+
+            let obj_clone = obj.clone();
+            let add_action = gio::SimpleAction::new("decrypt-password", None);
+            add_action.connect_activate(move |_, _| obj_clone.open_text_editor());
+            obj.add_action(&add_action);
+
             let store = PassStore::default();
 
             // synchronize action
@@ -108,7 +173,7 @@ mod imp {
                         let before_semicolon = &message[..idx];
 
                         overlay.add_toast(adw::Toast::new(before_semicolon));
-                        println!("Failed to synchronize: {}", e);
+                        eprintln!("Failed to synchronize: {}", e);
                     }
                 }
                 obj_clone.imp().init_list(&store_clone);
@@ -122,27 +187,14 @@ mod imp {
             self.list.connect_row_activated(move |_, row| {
                 if let Some(inner) = row.child() {
                     if let Ok(label) = inner.downcast::<gtk::Label>() {
-                        let overlay = obj_clone.imp().toast_overlay.clone();
                         let path = label.text().to_string().replace(" / ", "/");
                         println!("Selected: {}", path);
-
-                        // Open the password using the path
-                        let mut store = PassStore::default();
-                        match store.get(&path) {
-                            Ok(item) => {
-                                overlay.add_toast(adw::Toast::new(&item.password));
-                            }
-                            Err(e) => {
-                                let message = e.to_string();
-                                let idx = message.find(';').unwrap_or(message.len());
-                                let before_semicolon = &message[..idx];
-
-                                overlay.add_toast(adw::Toast::new(before_semicolon));
-                                println!("Failed to open password: {}", e);
-                            }
-                        }
+                        obj_clone.set_path(path.clone());
+                        obj_clone.open_text_editor();
+                        return;
                     }
                 }
+                obj_clone.show_toast("Failed to open password");
             });
 
             // Real-time filter: hide/show rows based on search text
@@ -209,6 +261,83 @@ impl PasswordstoreWindow {
     }
 
     pub fn open_new_password(&self) {
-        println!("Open new password");
+        let text_view = self.imp().text_view.clone();
+        text_view.set_buffer(Some(&gtk::TextBuffer::new(None)));
+        text_view.set_editable(true);
+        let text_page = self.imp().text_page.clone();
+        if !text_page.is_visible() {
+            self.imp().nav.push(&text_page.clone());
+        }
+    }
+
+    pub fn ask_for_passphrase(&self) {
+        let ask_page = self.imp().ask_page.clone();
+        if ask_page.is_visible() {
+            return;
+        } else {
+            self.imp().nav.push(&ask_page.clone());
+        }
+    }
+
+    pub fn open_text_editor(&self) {
+        let path = self.get_path();
+        let passphrase = self.imp().password_entry.text().to_string();
+        if passphrase.is_empty() {
+            let ask_page = self.imp().ask_page.clone();
+            if !&ask_page.is_visible() {
+                self.imp().nav.push(&ask_page.clone());
+            }
+            self.show_toast("Passphrase cannot be empty");
+            return;
+        }
+
+        let mut store = PassStore::default();
+        if store.entry_exists(&path) {
+            self.show_toast("Password not found");
+            let list_page = self.imp().list_page.clone();
+            if !&list_page.is_visible() {
+                self.imp().nav.push(&list_page.clone());
+            }
+            return;
+        }
+
+        match store.get(&path, passphrase.as_str()) {
+            Ok(item) => {
+                println!("Password: {}", item.password);
+                // Pass item to the text view
+                // Add item.password to the first line of the text view
+                // Add the item.extra (a list of strings) after that.
+                let mut text = item.password;
+                for line in item.extra {
+                    text.push_str(&format!("\n{}", line));
+                }
+                let buffer = gtk::TextBuffer::new(None);
+                buffer.set_text(&text);
+                let text_view = self.imp().text_view.clone();
+                text_view.set_buffer(Some(&buffer));
+                text_view.set_editable(false);
+                // Open the text page so that I can view (or edit) the encqrypted password file
+                self.imp().nav.push(&self.imp().text_page.clone());
+            }
+            Err(e) => {
+                let message = e.to_string();
+                let idx = message.find(';').unwrap_or(message.len());
+                let before_semicolon = &message[..idx];
+                self.show_toast(before_semicolon);
+                eprintln!("Failed to open password: {}", e);
+            }
+        }
+    }
+
+    pub fn get_path(&self) -> String {
+        self.imp().get_path()
+    }
+
+    pub fn set_path(&self, path: String) {
+        self.imp().set_path(path.clone());
+    }
+
+    pub fn show_toast(&self, message: &str) {
+        self.imp().show_toast(message);
     }
 }

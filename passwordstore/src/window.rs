537a9bd30986fd384e49bef1f104c3dfce060331
nick <noobping@users.noreply.github.com>
2025-05-10 21:05:37 +0200

    Add loading indicators during synchronization process

--- passwordstore/src/window.rs
+++ passwordstore/src/window.rs
@@ -336,6 +336,7 @@ mod imp {
                 let sync_action = gio::SimpleAction::new("synchronize", None);
                 let store_clone = store.clone();
                 sync_action.connect_activate(move |_, _| {
+                    obj_clone2.start_loading();
                     println!("Synchronizing...");
                     match store_clone.sync() {
                         Ok(_) => overlay.add_toast(adw::Toast::new("Synchronized successfully")),
@@ -348,6 +349,7 @@ mod imp {
                             eprintln!("Failed to synchronize: {}", e);
                         }
                     }
+                    obj_clone2.stop_loading();
                     obj_clone2.imp().init_list(&store_clone);
                 });
                 obj_clone.add_action(&sync_action);

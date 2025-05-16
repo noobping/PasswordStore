1068a637f489fa7ca7545ff82df6b279fdf04741
nick <noobping@users.noreply.github.com>
2025-05-16 17:16:25 +0200

    Update keyboard shortcut action name for saving passwords to reflect selected password functionality

--- passwordstore/src/application.rs
+++ passwordstore/src/application.rs
@@ -51,7 +51,7 @@ mod imp {
             obj.set_accels_for_action("win.new-password-path", &["<primary>n"]);
             obj.set_accels_for_action("win.git-url", &["<primary>g"]);
             obj.set_accels_for_action("win.remove-selected-password", &["<primary>d"]);
-            obj.set_accels_for_action("win.save-password", &["<primary>w"]);
+            obj.set_accels_for_action("win.save-selected-password", &["<primary>w"]);
             obj.set_accels_for_action("win.synchronize", &["<primary>s"]);
             obj.set_accels_for_action("win.toggle-search", &["<primary>f"]);
         }

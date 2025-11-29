2b0f41938ebbbdbe5c147697bfbd7553c674a787
nick <noobping@users.noreply.github.com>
2025-11-29 14:43:41 +0100

    dev id

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,7 @@
+#[cfg(debug_assertions)]
+pub const APP_ID: &str = concat!("dev.noobping.", env!("CARGO_PKG_NAME"), "-dev");
+
+#[cfg(not(debug_assertions))]
 pub const APP_ID: &str = concat!("dev.noobping.", env!("CARGO_PKG_NAME"));
 
 #[cfg(feature = "setup")]

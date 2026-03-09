6cc136d784c6441d1e16d12abe0c3df0c788d542
nick <noobping@users.noreply.github.com>
2026-03-09 12:26:40 +0100

    shorter message

--- src/window_messages.rs
+++ src/window_messages.rs
@@ -1,9 +1,3 @@
-#[cfg(not(feature = "flatpak"))]
-pub(crate) fn with_logs_hint(message: &str) -> String {
-    format!("{message} Open Logs for details.")
-}
-
-#[cfg(feature = "flatpak")]
 pub(crate) fn with_logs_hint(message: &str) -> String {
     message.to_string()
 }

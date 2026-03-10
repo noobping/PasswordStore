2adf83120fbaf2336a0abbcbb68a42d46c1cbb1f
nick <noobping@users.noreply.github.com>
2026-03-10 14:21:24 +0100

    unused

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,6 @@
-#[allow(dead_code)]
 #[cfg(debug_assertions)]
 pub const APP_ID: &str = concat!("io.github.noobping.", env!("CARGO_PKG_NAME"), ".develop");
 
-#[allow(dead_code)]
 #[cfg(not(debug_assertions))]
 pub const APP_ID: &str = concat!("io.github.noobping.", env!("CARGO_PKG_NAME"));
 
